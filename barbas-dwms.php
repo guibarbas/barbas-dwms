75fab8c77eca9afe1750fad8cc5f3db7ce8a22f9
Guilherme Souza <guilherme@barbas.digital>
2020-04-04 20:08:40 -0300

    Wordpress version 5.4 compatibility

--- barbas-dwms.php
+++ barbas-dwms.php
@@ -1,9 +1,9 @@
 <?php
 /*
 Plugin Name: Barbas - Default wp mail sender
-Plugin URI: http://www.barbas.digital
+Plugin URI: https://www.barbas.digital
 Description: Simple way to change the default wordpress sender's name and email.
-Version: 1.0.2.1
+Version: 1.0.3
 Author: Guilherme Souza
 Author URI: https://www.barbas.digital
 Licence: GPLv2 or later
